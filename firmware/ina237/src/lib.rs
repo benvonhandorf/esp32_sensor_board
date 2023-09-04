e1e0d37258a48dfa8a83498e8502a720ae4e3a35
Ben Von Handorf <ben@skyironstudio.com>
2023-09-04 10:09:22 -0400

    WIP INA237

--- firmware/ina237/src/lib.rs
+++ firmware/ina237/src/lib.rs
@@ -1,19 +1,6 @@
-#![no_std]
+// #![no_std]
+extern crate std;
 
 pub mod ina237;
 pub mod types;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- firmware/ina237/src/types.rs
+++ firmware/ina237/src/types.rs
@@ -23,46 +23,46 @@ pub enum AdcRange {
 
 pub enum Mode {
     SHUTDOWN = 0x00,
-    TRIGGERED_BUS_VOLTAGE_SS = 0x01,
-    TRIGGERED_SHUNT_VOLTAGE_SS = 0x02,
-    TRIGGERED_SHUNT_BUS_VOLTAGE_SS = 0x03,
-    TRIGGERED_TEMP_SS = 0x04,
-    TRIGGERED_TEMP_BUS_VOLTAGE_SS = 0x05,
-    TRIGGERED_TEMP_SHUNT_VOLTAGE_SS = 0x06,
-    TRIGGERED_TEMP_SHUNT_BUS_VOLTAGE_SS = 0x07,
-    SHUTDOWN_08 = 0x08,
-    CONTINUOUS_BUS_VOLTAGE = 0x09,
-    CONTINUOUS_SHUNT_VOLTAGE = 0x0A,
-    CONTINUOUS_SHUNT_BUS_VOLTAGE = 0x0B,
-    CONTINUOUS_TEMP = 0x0C,
-    CONTINUOUS_TEMP_BUS_VOLTAGE = 0x0D,
-    CONTINUOUS_TEMP_SHUNT_VOLTAGE = 0x0E,
-    CONTINUOUS_TEMP_SHUNT_BUS_VOLTAGE = 0x0F,
+    TriggeredBusVoltageSs = 0x01,
+    TriggeredShuntVoltageSs = 0x02,
+    TriggeredShuntBusVoltageSs = 0x03,
+    TriggeredTempSs = 0x04,
+    TriggeredTempBusVoltageSs = 0x05,
+    TriggeredTempShuntVoltageSs = 0x06,
+    TriggeredTempShuntBusVoltageSs = 0x07,
+    Shutdown08 = 0x08,
+    ContinuousBusVoltage = 0x09,
+    ContinuousShuntVoltage = 0x0A,
+    ContinuousShuntBusVoltage = 0x0B,
+    ContinuousTemp = 0x0C,
+    ContinuousTempBusVoltage = 0x0D,
+    ContinuousTempShuntVoltage = 0x0E,
+    ContinuousTempShuntBusVoltage = 0x0F,
 }
 
 pub enum ConversionTime {
-    50_us = 0x00,
-    84_us = 0x01,
-    150_us = 0x02,
-    280_us = 0x03,
-    540_us = 0x04,
-    1052_us = 0x05,
-    2074_us = 0x06,
-    4120_us = 0x07,
+    DurationUs50 = 0x00,
+    DurationUs84 = 0x01,
+    DurationUs150 = 0x02,
+    DurationUs280 = 0x03,
+    DurationUs540 = 0x04,
+    DurationUs1052 = 0x05,
+    DurationUs2074 = 0x06,
+    DurationUs4120 = 0x07,
 }
 
 pub enum AdcAveraging {
-    AVG_1 = 0x00,
-    AVG_4 = 0x01,
-    AVG_16 = 0x02,
-    AVG_64 = 0x03,
-    AVG_128 = 0x04,
-    AVG_256 = 0x05,
-    AVG_512 = 0x06,
-    AVG_1024 = 0x07,
+    Avg1 = 0x00,
+    Avg4 = 0x01,
+    Avg16 = 0x02,
+    Avg64 = 0x03,
+    Avg128 = 0x04,
+    Avg256 = 0x05,
+    Avg512 = 0x06,
+    Avg1024 = 0x07,
 }
 
-pub struct ConfigurationValues {
+pub struct ConfigurationRegisterValues {
     /// Force sensor reset
     reset: bool,
     /// Conversion delay, 2ms steps.  Range 0ms - 510ms
@@ -76,29 +76,33 @@ pub struct ConfigurationValues {
     /// ADC Conversion Time for Shunt Voltage.  Default: 1052us
     shunt_voltage_conversion_time: ConversionTime,
     /// ADC Conversion Time for Temperature.  Default: 1052us
-    temp_conversion_time: ConversionTime,
+    temperature_conversion_time: ConversionTime,
     /// ADC Averaging
     adc_averaging: AdcAveraging,
 }
 
-impl ConfigurationValues {
-    fn new() -> ConfigurationValues {
-        ConfigurationValues {
-            reset = false,
-            conversion_delay = 0,
-            adc_range = AdcRange::HIGH,
-            mode: Mode::CONTINUOUS_TEMP_SHUNT_BUS_VOLTAGE,
-            bus_voltage_conversion_time = ConversionTime::1052_us,
-            shunt_voltage_conversion_time = ConversionTime::1052_us,
-            temperature_conversion_time = ConversionTime::1052_us,
-            adc_averaging = AdcAveraging::AVG_1,
+impl ConfigurationRegisterValues {
+    fn new() -> ConfigurationRegisterValues {
+        ConfigurationRegisterValues {
+            reset : false,
+            conversion_delay : 0,
+            adc_range : AdcRange::HIGH,
+            mode: Mode::ContinuousTempShuntBusVoltage,
+            bus_voltage_conversion_time : ConversionTime::DurationUs1052,
+            shunt_voltage_conversion_time : ConversionTime::DurationUs1052,
+            temperature_conversion_time : ConversionTime::DurationUs1052,
+            adc_averaging : AdcAveraging::Avg1,
         }
     }
 }
 
-impl Into<u16> ConfigurationValues {
+impl Into<u16> for ConfigurationRegisterValues {
     fn into(self) -> u16 {
-        0x00
+        0x0000 | if self.reset {
+            0x8000
+        } else {
+            0x0000
+        }
     }
 }
 
@@ -110,16 +114,23 @@ mod tests {
     fn configuration_constructor_sets_values() {
         let result = Configuration::new(0x01, 2000);
 
-        assert_eq!(0x01, result.address);
         assert_eq!(2000, result.shunt_cal);
     }
 
     #[test]
     fn configuration_values_into_u16_reset_false() {
-        let result = ConfigurationValues::new();
+        let configuration_register_values = ConfigurationRegisterValues::new();
+        let result: u16 = configuration_register_values.into();
 
-        assert_eq!(0x01, result.address);
-        assert_eq!(2000, result.shunt_cal);
+        assert_eq!(0x0000, 0x8000 & result);
+    }
+
+    #[test]
+    fn configuration_values_with_reset_into_u16_reset_true() {
+        let configuration_register_values = ConfigurationRegisterValues::new();
+        let result: u16 = configuration_register_values.into();
+
+        assert_eq!(0x8000, 0x8000 & result);
     }
 }
 
